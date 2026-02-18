acdc6db6ff1f1957b8e471c93964d2b7173fd614
Pablo Duarte <pablocdds@gmail.com>
2026-02-18 06:54:59 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -48,19 +48,13 @@
  * The mainPage value must match a key in the PAGES object exactly.
  */
 import AuditoriaDodia from './pages/AuditoriaDodia';
-import BancoVirtual from './pages/BancoVirtual';
-import Categorias from './pages/Categorias';
 import Checklists from './pages/Checklists';
-import Cofres from './pages/Cofres';
 import ContagemTarefa from './pages/ContagemTarefa';
 import Contagens from './pages/Contagens';
-import ContasBancarias from './pages/ContasBancarias';
 import ContasPagar from './pages/ContasPagar';
-import ContasReceber from './pages/ContasReceber';
 import DRE from './pages/DRE';
 import Dashboard from './pages/Dashboard';
 import Estoque from './pages/Estoque';
-import Fornecedores from './pages/Fornecedores';
 import IAExecutora from './pages/IAExecutora';
 import Lojas from './pages/Lojas';
 import Movimentacoes from './pages/Movimentacoes';
@@ -75,26 +69,18 @@ import Vendas from './pages/Vendas';
 import Onboarding from './pages/Onboarding';
 import AdminSaaS from './pages/AdminSaaS';
 import PDVMobile from './pages/PDVMobile';
-import Empresas from './pages/Empresas';
-import Notificacoes from './pages/Notificacoes';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "AuditoriaDodia": AuditoriaDodia,
-    "BancoVirtual": BancoVirtual,
-    "Categorias": Categorias,
     "Checklists": Checklists,
-    "Cofres": Cofres,
     "ContagemTarefa": ContagemTarefa,
     "Contagens": Contagens,
-    "ContasBancarias": ContasBancarias,
     "ContasPagar": ContasPagar,
-    "ContasReceber": ContasReceber,
     "DRE": DRE,
     "Dashboard": Dashboard,
     "Estoque": Estoque,
-    "Fornecedores": Fornecedores,
     "IAExecutora": IAExecutora,
     "Lojas": Lojas,
     "Movimentacoes": Movimentacoes,
@@ -109,8 +95,6 @@ export const PAGES = {
     "Onboarding": Onboarding,
     "AdminSaaS": AdminSaaS,
     "PDVMobile": PDVMobile,
-    "Empresas": Empresas,
-    "Notificacoes": Notificacoes,
 }
 
 export const pagesConfig = {
