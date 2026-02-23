aa8891ce610f7fb8d8eb47b2f68ec3f57ef301e5
Pablo Duarte <pablocdds@gmail.com>
2026-02-23 12:26:44 +0000

    File changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -203,7 +203,7 @@ export default function Sidebar({ collapsed, onToggle }) {
                 </Link>
               ))}
             </div>
-          </div>
+            </div>
           );
         })}
       </nav>
